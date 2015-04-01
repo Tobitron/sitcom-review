a78fcf7f15d6b27d4479450e5189793cb6b76210
Kate Buckner <ingidora@gmail.com>
2015-04-01 09:46:14 -0400

    Fixed a validation in review.rb that was causing tests to fail.

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,9 +6,7 @@ class Review < ActiveRecord::Base
   numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   validates :user, presence: true
   validates :sitcom, presence: true
-  validates :user_id, uniqueness: true,
-                      scope: [:sitcom_id],
-                      message: "You have already reviewed this show."
+  validates_uniqueness_of :user_id, scope: [:sitcom_id], message: "You have already reviewed this show."
 
   def owner?(current_user)
     user == current_user
