7ea8bedc5467b72a4dc117dbfaedda66ff8540a1
Rocco Basso <basso.r@husky.neu.edu>
2015-04-01 10:38:42 -0400

    added edit and delete test coverage

--- spec/features/destroy_sitcom_spec.rb
+++ spec/features/destroy_sitcom_spec.rb
@@ -24,5 +24,21 @@ feature 'user deletes sitcom' do
 
       expect(page).to_not have_content('The Simpsons')
     end
+
+    scenario 'user deletes a sitcom they submitted' do
+      visit new_sitcom_path
+
+      fill_in 'sitcom_name', with: 'The Simpsons'
+      fill_in 'sitcom_description', with: 'this is a description for the simpsons'
+      select 1995, from: 'sitcom_start_year'
+      fill_in 'sitcom_genre', with: 'Cartoon Comedy'
+      fill_in 'sitcom_network', with: 'Fox'
+
+      click_on 'Create Sitcom'
+      click_on 'Sign Out'
+      click_on 'Simpsons'
+
+      expect(page).to_not have_content('Delete')
+    end
   end
 end
--- spec/features/edit_sitcom_spec.rb
+++ spec/features/edit_sitcom_spec.rb
@@ -9,7 +9,7 @@ feature 'user edits sitcom' do
       sign_in_as user
     end
 
-    scenario 'user edits a sitcom they submitted' do
+    scenario 'user edits a sitcom they submitted with valid information' do
       visit new_sitcom_path
 
       fill_in 'sitcom_name', with: 'The Simpsons'
@@ -27,6 +27,26 @@ feature 'user edits sitcom' do
       click_on 'Update Sitcom'
 
       expect(page).to have_content('THIS IS A BRAND NEW DESCRIPTION FOR THE SIMPSONS')
+    end
+
+    scenario 'user edits a sitcom they submitted with invaild information' do
+      visit new_sitcom_path
+
+      fill_in 'sitcom_name', with: 'The Simpsons'
+      fill_in 'sitcom_description', with: 'this is a description for the simpsons'
+      select 1995, from: 'sitcom_start_year'
+      fill_in 'sitcom_genre', with: 'Cartoon Comedy'
+      fill_in 'sitcom_network', with: 'Fox'
+
+      click_on 'Create Sitcom'
+
+      click_on 'Edit'
+
+      fill_in 'sitcom_name', with: ''
+
+      click_on 'Update Sitcom'
+
+      expect(page).to have_content('Error: Sitcom not update')
     end
   end
 end
