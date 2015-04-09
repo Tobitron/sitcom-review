1f910d2bea684e537aeffd416d2900d7e7539be3
Rocco Basso <basso.r@husky.neu.edu>
2015-04-09 15:59:07 -0400

    Refactored

--- app/models/sitcom_manager.rb
+++ app/models/sitcom_manager.rb
@@ -7,36 +7,65 @@ class SitcomManager
   end
 
   def newest_sitcoms
-    if search_params.nil?
-      Sitcom.order(start_year: :desc).page page_params
+    if user_searched?
+      order_search_by_start_year
     else
-      Sitcom.search(search_params).order(start_year: :desc).page page_params
+      order_by_start_year
     end
   end
 
   def highest_rated
-    if search_params.nil?
-      highest_rated = Sitcom.all.sort_by do |sitcom|
-        sitcom.reviews.average(:rating).round(2).to_f unless sitcom.reviews.average(:rating).nil?
-      end
+    if user_searched?
+      highest_rated = order_search_by_highest_rated
     else
-      highest_rated = Sitcom.search(search_params).sort_by do |sitcom|
-        sitcom.reviews.average(:rating).round(2).to_f unless sitcom.reviews.average(:rating).nil?
-      end
+      highest_rated = order_by_highest_rated
     end
     Kaminari.paginate_array(highest_rated.reverse).page page_params
   end
 
   def most_reviewed
-    if search_params.nil?
-      most_reviewed = Sitcom.all.sort_by do |sitcom|
-        sitcom.reviews.count
-      end
+    if user_searched?
+      most_reviewed = order_search_by_most_reviewed
     else
-      most_reviewed = Sitcom.search(search_params).sort_by do |sitcom|
-        sitcom.reviews.count
-      end
+      most_reviewed = order_by_most_reviewed
     end
     Kaminari.paginate_array(most_reviewed.reverse).page page_params
+  end
+
+  private
+  def user_searched?
+    !(search_params.nil?)
+  end
+
+  def order_by_start_year
+    Sitcom.order(start_year: :desc).page page_params
+  end
+
+  def order_search_by_start_year
+    Sitcom.search(search_params).order(start_year: :desc).page page_params
+  end
+
+  def order_search_by_highest_rated
+    Sitcom.search(search_params).sort_by do |sitcom|
+      sitcom.reviews.average(:rating).round(2).to_f unless sitcom.reviews.average(:rating).nil?
+    end
+  end
+
+  def order_by_highest_rated
+    Sitcom.all.sort_by do |sitcom|
+      sitcom.reviews.average(:rating).round(2).to_f unless sitcom.reviews.average(:rating).nil?
+    end
+  end
+
+  def order_search_by_most_reviewed
+    Sitcom.search(search_params).sort_by do |sitcom|
+      sitcom.reviews.count
+    end
+  end
+
+  def order_by_most_reviewed
+    Sitcom.all.sort_by do |sitcom|
+      sitcom.reviews.count
+    end
   end
 end
