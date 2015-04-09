39756d2ce38b6b93659bf0a87a007e3648f62125
Rocco Basso <basso.r@husky.neu.edu>
2015-04-09 14:27:35 -0400

    made several queries to display data in defferent sorts on homepage and SitcomsManager to handle most of the logic

--- app/controllers/sitcoms_controller.rb
+++ app/controllers/sitcoms_controller.rb
@@ -4,9 +4,22 @@ class SitcomsController < ApplicationController
 
   def index
     unless params[:search] == nil
+      sitcom_manager_with_search = SitcomManager.new(params[:search], params[:page])
+
       @sitcoms = Sitcom.search(params[:search]).page params[:page]
+
+      @newest_sitcoms = sitcom_manager_with_search.newest_sitcoms
+      @highest_rated = sitcom_manager_with_search.highest_rated
+      @most_reviewed = sitcom_manager_with_search.most_reviewed
     else
+      sitcom_manager_without_search = SitcomManager.new(nil, params[:page])
+
       @sitcoms = Sitcom.order(start_year: :desc).page params[:page]
+
+      @newest_sitcoms = sitcom_manager_without_search.newest_sitcoms
+      @highest_rated = sitcom_manager_without_search.highest_rated
+      @most_reviewed = sitcom_manager_without_search.most_reviewed
+      binding.pry
     end
   end
 
---
+++ app/models/sitcom_manager.rb
@@ -0,0 +1,42 @@
+class SitcomManager
+  attr_reader :search_params, :page_params
+
+  def initialize(search_params, page)
+    @search_params = search_params
+    @page_params = page_params
+  end
+
+  def newest_sitcoms
+    if search_params.nil?
+      Sitcom.order(start_year: :desc).page page_params
+    else
+      Sitcom.search(search_params).order(start_year: :desc).page page_params
+    end
+  end
+
+  def highest_rated
+    if search_params.nil?
+      highest_rated = Sitcom.all.sort_by do |sitcom|
+        sitcom.reviews.average(:rating).round(2).to_f
+      end
+    else
+      highest_rated = Sitcom.search(search_params).sort_by do |sitcom|
+        sitcom.reviews.average(:rating).round(2).to_f
+      end
+    end
+    Kaminari.paginate_array(highest_rated.reverse).page page_params
+  end
+
+  def most_reviewed
+    if search_params.nil?
+      most_reviewed = Sitcom.all.sort_by do |sitcom|
+        sitcom.reviews.count
+      end
+    else
+      most_reviewed = Sitcom.search(search_params).sort_by do |sitcom|
+        sitcom.reviews.count
+      end
+    end
+    Kaminari.paginate_array(most_reviewed.reverse).page page_params
+  end
+end
