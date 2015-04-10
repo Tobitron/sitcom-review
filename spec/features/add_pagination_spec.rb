795c212b92c2a8f6a32740f6fc99b7ee7c753d11
Tobias Kahn <tobiasbkahn@gmail.com>
2015-04-10 12:11:46 -0400

    Fix test errors

--- spec/features/add_pagination_spec.rb
+++ spec/features/add_pagination_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 feature 'any user views a list of all sitcoms with pagination' do
 
   scenario 'I view paginated sitcoms' do
+    FactoryGirl.create(:sitcom, name: 'Archer', start_year: 2015)
     FactoryGirl.create_list(:sitcom, 25)
-    FactoryGirl.create(:sitcom, name: 'Archer')
 
     visit root_path
-
-    expect(page).to_not have_content('Archer')
+    expect(page).to have_content('Archer')
   end
 
   scenario 'I view paginated sitcoms page 2' do
@@ -16,7 +15,6 @@ feature 'any user views a list of all sitcoms with pagination' do
     FactoryGirl.create(:sitcom, name: 'Archer')
 
     visit root_path
-
     click_on 'Next'
 
     expect(page).to have_content('Archer')
